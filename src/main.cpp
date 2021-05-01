f71925310807091dce46d551b93ccb8881c496e6
gdescomps <46891683+gdescomps@users.noreply.github.com>
2021-05-02 01:41:20 +0200

    draw function

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,68 @@
 #define TIME_PER_FRAME_MS  (1.0f/FRAMERATE * 1e3)
 #define INDICE_TO_PTR(x) ((void*)(x))
 
+
+void draw(Shader* shader, Geometry& object, float& angleSquare){
+
+    glm::mat4 cameraMatrix(1.0f); //Camera matrix. If you want a 3D projection matrix, look at
+    // ,→ glm::lookAt : glm::mat4 mat = glm::lookAt(EyePosition, Center, UpVector) where each
+    // ,→ parameters is typed glm::vec3 : glm::vec3 vec(x, y, z); (you can do directly in the
+    // ,→ parameters : glm::vec3(x, y, z) to create glm::vec3 on the fly)
+
+    glm::mat4 matrix(1.0f); //Defines an identity matrix
+    // 1 0 0 0
+    // 0 1 0 0
+    // 0 0 1 0
+    // 0 0 0 1
+
+    //The most left transformation presented in equation (1) has to be done first
+    // matrix = glm::translate(matrix, glm::vec3(transX, transY, transZ)); //We translate
+    //  //We rotate via an
+    // ,→ axis and an angle around this axis
+
+    // matrix = glm::scale(matrix, glm::vec3(0.5f, 0.5f, 1.0f)); //And then we scale
+    // matrix = glm::translate(matrix, glm::vec3(0.5f, 0, 0)); //We translate
+    matrix = glm::rotate(matrix, glm::radians(angleSquare), glm::vec3(0, 1, 0));
+    matrix = glm::rotate(matrix, glm::radians(angleSquare), glm::vec3(1, 0, 0));
+
+    angleSquare >= 360 ? angleSquare=0 : angleSquare++; 
+
+    glm::mat4 mvp = cameraMatrix * matrix; //Finally we multiply all the matrices. In C++ you
+    // ,→ can do this because glm::mat4 has redefined the operator* to work with glm::mat4
+    // ,→ objects (and even with a glm::mat4 and a glm::vec4).
+
+    glUseProgram(shader->getProgramID());
+        { //The brackets are useless but help at the clarity of the code
+
+            //Work with vPosition
+            GLint vPosition = glGetAttribLocation(shader->getProgramID(), "vPosition");
+            glVertexAttribPointer(vPosition, 3, GL_FLOAT, 0, 3*sizeof(float), 0); //It is here that you
+            // ,→ select how the Shader reads the VBO. Indeed the 5th parameter is called
+            // ,→ "stride" : it is the distance in bytes between two values for the same
+            // ,→ kind or variable. If the values are side-by-side, stride == 0. Here we
+            // ,→ need to set to 3*sizeof(float) for the first version of the VBO seen in
+            // ,→ class.
+            glEnableVertexAttribArray(vPosition); //Enable "vPosition"
+            //Work with vColor
+            GLint vColor = glGetAttribLocation(shader->getProgramID(), "vNormal");
+            //Colors start at 9*sizeof(float) (3*nbVertices*sizeof(float)) for the second
+            // ,→ version of the VBO. For the first version of the VBO, both the stride
+            // ,→ and the offset should be 3*sizeof(float) here
+            glVertexAttribPointer(vColor, 3, GL_FLOAT, 0, 3*sizeof(float), INDICE_TO_PTR(object.getNbVertices()*3*sizeof(float))); //Convert an indice to void* : (void*)(x)
+            glEnableVertexAttribArray(vColor); //Enable"vColor"
+            glDrawArrays(GL_TRIANGLES, 0, object.getNbVertices()); //Draw the triangle (three points which
+            // ,→ starts at offset = 0 in the VBO). GL_TRIANGLES tells that we are reading
+            // ,→ three points per three points to form a triangle. Other kind of "
+            // ,→ reading" exist, see glDrawArrays for more details.
+            glBindBuffer(GL_ARRAY_BUFFER, 0); //Close the VBO (not mandatory but recommended,→ for not modifying it accidently).
+            
+            GLint uMVP = glGetUniformLocation(shader->getProgramID(), "uMVP"); //Get the "uScale" location (ID)
+            glUniformMatrix4fv(uMVP, 1, GL_FALSE, glm::value_ptr(mvp)); //Set "uScale" at 0.5f. Remark : we use glUniform1f for sending a
+            // ,→ (1) float (f). For other kinds of uniform (integers, vectors, etc.) see the
+            // ,→ documentation
+        }
+}
+
 int main(int argc, char *argv[])
 {
     ////////////////////////////////////////
@@ -138,65 +200,13 @@ int main(int argc, char *argv[])
         //Clear the screen : the depth buffer and the color buffer
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-        glm::mat4 cameraMatrix(1.0f); //Camera matrix. If you want a 3D projection matrix, look at
-        // ,→ glm::lookAt : glm::mat4 mat = glm::lookAt(EyePosition, Center, UpVector) where each
-        // ,→ parameters is typed glm::vec3 : glm::vec3 vec(x, y, z); (you can do directly in the
-        // ,→ parameters : glm::vec3(x, y, z) to create glm::vec3 on the fly)
+        
+        glBindBuffer(GL_ARRAY_BUFFER, myBuffer);
 
-        glm::mat4 matrix(1.0f); //Defines an identity matrix
-        // 1 0 0 0
-        // 0 1 0 0
-        // 0 0 1 0
-        // 0 0 0 1
-
-        //The most left transformation presented in equation (1) has to be done first
-        // matrix = glm::translate(matrix, glm::vec3(transX, transY, transZ)); //We translate
-        //  //We rotate via an
-        // ,→ axis and an angle around this axis
-
-        // matrix = glm::scale(matrix, glm::vec3(0.5f, 0.5f, 1.0f)); //And then we scale
-        // matrix = glm::translate(matrix, glm::vec3(0.5f, 0, 0)); //We translate
-        matrix = glm::rotate(matrix, glm::radians(angleSquare), glm::vec3(0, 1, 0));
-        matrix = glm::rotate(matrix, glm::radians(angleSquare), glm::vec3(1, 0, 0));
-
-        angleSquare >= 360 ? angleSquare=0 : angleSquare++; 
-
-        glm::mat4 mvp = cameraMatrix * matrix; //Finally we multiply all the matrices. In C++ you
-        // ,→ can do this because glm::mat4 has redefined the operator* to work with glm::mat4
-        // ,→ objects (and even with a glm::mat4 and a glm::vec4).
-
-
-        glUseProgram(shader->getProgramID());
-        { //The brackets are useless but help at the clarity of the code
-            glBindBuffer(GL_ARRAY_BUFFER, myBuffer);
-
-            //Work with vPosition
-            GLint vPosition = glGetAttribLocation(shader->getProgramID(), "vPosition");
-            glVertexAttribPointer(vPosition, 3, GL_FLOAT, 0, 3*sizeof(float), 0); //It is here that you
-            // ,→ select how the Shader reads the VBO. Indeed the 5th parameter is called
-            // ,→ "stride" : it is the distance in bytes between two values for the same
-            // ,→ kind or variable. If the values are side-by-side, stride == 0. Here we
-            // ,→ need to set to 3*sizeof(float) for the first version of the VBO seen in
-            // ,→ class.
-            glEnableVertexAttribArray(vPosition); //Enable "vPosition"
-            //Work with vColor
-            GLint vColor = glGetAttribLocation(shader->getProgramID(), "vNormal");
-            //Colors start at 9*sizeof(float) (3*nbVertices*sizeof(float)) for the second
-            // ,→ version of the VBO. For the first version of the VBO, both the stride
-            // ,→ and the offset should be 3*sizeof(float) here
-            glVertexAttribPointer(vColor, 3, GL_FLOAT, 0, 3*sizeof(float), INDICE_TO_PTR(cube.getNbVertices()*3*sizeof(float))); //Convert an indice to void* : (void*)(x)
-            glEnableVertexAttribArray(vColor); //Enable"vColor"
-            glDrawArrays(GL_TRIANGLES, 0, cube.getNbVertices()); //Draw the triangle (three points which
-            // ,→ starts at offset = 0 in the VBO). GL_TRIANGLES tells that we are reading
-            // ,→ three points per three points to form a triangle. Other kind of "
-            // ,→ reading" exist, see glDrawArrays for more details.
-            glBindBuffer(GL_ARRAY_BUFFER, 0); //Close the VBO (not mandatory but recommended,→ for not modifying it accidently).
+        draw(shader, cube, angleSquare);
             
-            GLint uMVP = glGetUniformLocation(shader->getProgramID(), "uMVP"); //Get the "uScale" location (ID)
-            glUniformMatrix4fv(uMVP, 1, GL_FALSE, glm::value_ptr(mvp)); //Set "uScale" at 0.5f. Remark : we use glUniform1f for sending a
-            // ,→ (1) float (f). For other kinds of uniform (integers, vectors, etc.) see the
-            // ,→ documentation
-        }
+        glBindBuffer(GL_ARRAY_BUFFER, 0); //Close the VBO (not mandatory but recommended,→ for not modifying it accidently).
+            
         glUseProgram(0); //Close the program. This is heavy for the GPU. In reality we do this
         // ,→ only if we have to CHANGE the shader (hence we cache the current in-use shader)
         // ,→ . For this course however the performances are not so important.
